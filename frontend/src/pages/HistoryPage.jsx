9369e5259e38300008e2b64cdcbf37855a9dc70b
Jonathan Saravia Zurita <jonathan.saravia.223@gmail.com>
2025-05-14 22:38:11 -0400

    Agregado frontend como carpeta normal

---
+++ frontend/src/pages/HistoryPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+const HistoryPage = () => {
+    return (
+        <h1>Texto HistoryPage</h1>
+    )
+}
+
+export default HistoryPage;
