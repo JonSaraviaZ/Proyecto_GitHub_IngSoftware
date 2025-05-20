0f4fc4743aa70eb7321639069d3485e7d4c21125
xDaniel-R <ce.caceres@duocuc.cl>
2025-05-20 00:32:22 -0400

    feat: actualizaciones en login y register.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,20 @@
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './pages/LoginPage/LoginPage';
+import LoginPage from './pages/LoginPage/LoginPage';
 import HomePage from './pages/HomePage/HomePage';
 import HistoryPage from './pages/HistoryPage/HistoryPage';
 import RegisterPage from './pages/RegisterPage/RegisterPage';
 import RegisterExitoso from './pages/RegisterExitoso/RegisterExitoso';
 
 function App() {
+  const [, setToken] = useState(null); // <-- Agregado
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Navigate to="/Login" replace />} />
         <Route path="/HomePage" element={<HomePage />} />
-        <Route path="/Login" element={<Login />} />
+        <Route path="/Login" element={<LoginPage setToken={setToken} />} /> {/* <-- Corregido */}
         <Route path="/RegisterPage" element={<RegisterPage />} />
         <Route path="/RegisterExitoso" element={<RegisterExitoso />} />
         <Route path="/HistoryPage" element={<HistoryPage />} />
@@ -21,4 +24,3 @@ function App() {
 }
 
 export default App;
-
--- frontend/src/pages/LoginPage/LoginPage.jsx
+++ frontend/src/pages/LoginPage/LoginPage.jsx
@@ -1,19 +1,12 @@
 import React from 'react';
 import styles from './LoginPage.module.css';
-import { Link } from "react-router-dom";
-import { useState } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
 import Login from '../../components/Login/Login';
 
 const LoginPage = ({ setToken }) => {
-
   return (
-      <div className={styles.formContainer}> 
-        <Login></Login>
-      </div>
-        
-    
+    <div className={styles.formContainer}>
+      <Login setToken={setToken} /> {/* <-- Corregido */}
+    </div>
   );
 };
 
